4b93a1a7fd4b50ec9a2b04432fcff8ce6d4a8615
Stanislav Stehniy <sstehniy@gmail.com>
2023-05-10 23:15:14 +0200

    refactor the code in scd.go

--- pkg/scd/scd.go
+++ pkg/scd/scd.go
@@ -1,7 +1,6 @@
 package scd
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -11,8 +10,12 @@ import (
 	"time"
 
 	"github.com/go-rod/rod"
-	"github.com/go-rod/rod/lib/launcher"
 	"github.com/schollz/progressbar/v3"
+)
+
+const (
+	SoundCloudSearchURL = "https://soundcloud.com/search/sounds?q="
+	SoundCloudBaseURL   = "https://soundcloud.com"
 )
 
 type SongData struct {
@@ -27,6 +30,26 @@ type FetchResponse struct {
 	index int
 }
 
+func downloadChunk(url string, index int, arr *[]FetchResponse, bar *progressbar.ProgressBar) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Printf("failed to fetch the track: %v", err)
+		return
+	}
+	defer resp.Body.Close()
+	bar.Add(1)
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("failed to read bytes from the response: %v", err)
+		return
+	}
+	(*arr)[index] = FetchResponse{
+		data:  data,
+		index: index,
+	}
+
+}
+
 func downloadChunks(urls []string) *[]FetchResponse {
 	bar := progressbar.Default(int64(len(urls)), "Track is being downloaded")
 	bytesData := make([]FetchResponse, len(urls))
@@ -34,128 +57,91 @@ func downloadChunks(urls []string) *[]FetchResponse {
 	var wg sync.WaitGroup
 	for index, url := range urls {
 		wg.Add(1)
-		go func(url string, index int, arr *[]FetchResponse) {
+
+		go func(url string, index int) {
 			defer wg.Done()
-			resp, err := http.Get(url)
-			if err != nil {
-				log.Printf("failed to fetch the track: %v", err)
-				return
-			}
-			defer resp.Body.Close()
-			data, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				log.Printf("failed to read bytes from the response: %v", err)
-				return
-			}
-			(*arr)[index] = FetchResponse{
-				data:  data,
-				index: index,
-			}
-			bar.Add(1)
-		}(url, index, &bytesData)
+			downloadChunk(url, index, &bytesData, bar)
+		}(url, index)
 	}
 
 	wg.Wait()
-	bar.Close()
 
 	return &bytesData
 }
 
-// SearchSongsByTitle searches for songs by title
 func SearchSongsByTitle(searchString string) []SongData {
-	output := []SongData{}
-
 	browser := rod.New().MustConnect()
 	defer browser.MustClose()
-	page := browser.MustPage("https://soundcloud.com/search/sounds?q=" + strings.Trim(searchString, " "))
 
+	page := browser.MustPage(SoundCloudSearchURL + strings.Trim(searchString, " "))
 	page.MustWaitRequestIdle()()
-
 	page.MustElement("#onetrust-accept-btn-handler").MustClick()
-
 	page.MustWait(`()=>document.querySelector(".onetrust-pc-dark-filter").style.display == "none"`)
 
 	if _, err := page.Timeout(500 * time.Microsecond).Element(`.sc-type-large.sc-text-h3.sc-text-light.sc-text-primary.searchList__emptyText`); err == nil {
-		return output
+		return []SongData{}
 	}
 
 	listItems := page.MustElementsByJS(`() => document.querySelectorAll(".searchList__item")`)
+	return createSongDataFromList(listItems)
+}
 
+func createSongDataFromList(listItems []*rod.Element) []SongData {
+	output := []SongData{}
 	for _, item := range listItems {
-
 		_, err := item.Element(".sc-button-play.playButton.sc-button.sc-button-xlarge.sc-button-disabled")
 		isDisabled := err != nil
 
 		output = append(output, SongData{
 			Title:     item.MustElement(".sc-link-primary.soundTitle__title.sc-link-dark.sc-text-h4").MustText(),
 			Author:    item.MustElement(".soundTitle__usernameText").MustText(),
-			Url:       "https://soundcloud.com" + *item.MustElement(".sc-link-primary.soundTitle__title.sc-link-dark.sc-text-h4").MustAttribute("href"),
+			Url:       SoundCloudBaseURL + *item.MustElement(".sc-link-primary.soundTitle__title.sc-link-dark.sc-text-h4").MustAttribute("href"),
 			Available: isDisabled,
 		})
-
 	}
 	return output
 }
 
-func DownloadTrackByUrl(songData *SongData) {
-	ln := launcher.New().
-		Set("no-sandbox", "true").
-		Headless(true).
-		Set("disable-notifications")
-
-	ctl, err := ln.Launch()
-	if err != nil {
-		log.Println("cannot init launcher", err)
-		return
-	}
-
-	browser := rod.New().
-		ControlURL(ctl)
-
-	err = browser.Connect()
-	if err != nil {
-		log.Println("cannot connect to browser", err)
-		return
-	}
-	defer browser.MustClose()
-
+func hijackRequests(browser *rod.Browser, hijackedUrl *[]string, wg *sync.WaitGroup) {
 	router := browser.HijackRequests()
-	var hijackedUrl []string
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-
 	router.MustAdd("*", func(ctx *rod.Hijack) {
-		ctx.LoadResponse(
-			&http.Client{}, true)
+		ctx.LoadResponse(&http.Client{}, true)
 		if strings.Contains(ctx.Request.URL().String(), "m3u8") {
-			fmt.Println("found m3u8")
-			hijackedUrl = regexp.MustCompile(`(https?:\/\/[^\s]+)`).FindAllString(ctx.Response.Body(), -1)
+			*hijackedUrl = regexp.MustCompile(`(https?:\/\/[^\s]+)`).FindAllString(ctx.Response.Body(), -1)
 			router.MustRemove("*")
 			router.MustStop()
 			wg.Done()
 		}
 	})
 	go router.Run()
+}
 
-	var page *rod.Page
-	err = rod.Try(func() {
-		page = browser.
-			MustPage(songData.Url).
-			MustSetViewport(1366, 748, 1, false).
-			MustWindowMaximize()
-	})
+func setupPage(browser *rod.Browser, songData *SongData) (*rod.Page, error) {
+	page := browser.MustPage(songData.Url)
 
-	if err != nil {
-		log.Println("cannot open page", err)
-		return
-	}
-	defer page.MustClose()
-	page.MustWaitLoad()
+	page.MustSetViewport(1366, 748, 1, false).MustWindowMaximize().MustWaitLoad()
 	wait := page.MustWaitRequestIdle()
 	wait()
 
-	page.MustScreenshot("screenshot.png")
+	return page, nil
+}
 
+func DownloadTrackByUrl(songData *SongData) {
+	browser := setupBrowser()
+
+	defer browser.MustClose()
+
+	hijackedUrl := []string{}
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	hijackRequests(browser, &hijackedUrl, wg)
+
+	page, err := setupPage(browser, songData)
+	if err != nil {
+		return
+	}
+	defer page.MustClose()
 	wg.Wait()
 	chunks := *downloadChunks(hijackedUrl)
 	rawBytes := []byte{}
