c69dc2cc1d392619452d3b383ec92af8550eee50
Stanislav Stehniy <sstehniy@gmail.com>
2023-05-13 21:01:35 +0200

    create types file

---
+++ pkg/scd/types.go
@@ -0,0 +1,20 @@
+package scd
+
+type SongData struct {
+	Title     string
+	Author    string
+	Url       string
+	Available bool
+}
+
+type PlaylistData struct {
+	Title      string
+	Author     string
+	Url        string
+	TrackCount int
+}
+
+type FetchResponse struct {
+	data  []byte
+	index int
+}
