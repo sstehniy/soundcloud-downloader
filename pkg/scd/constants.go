8588ea7c29aef5e29d7ee4d7a05ed69eaa169ba8
Stanislav Stehniy <sstehniy@gmail.com>
2023-05-13 21:01:22 +0200

    create constants file

---
+++ pkg/scd/constants.go
@@ -0,0 +1,8 @@
+// constants.go
+package scd
+
+const (
+	SoundCloudSongSearchURL     = "https://soundcloud.com/search/sounds?q="
+	SoundCloudPlaylistSearchURL = "https://soundcloud.com/search/sets?q="
+	SoundCloudBaseURL           = "https://soundcloud.com"
+)
